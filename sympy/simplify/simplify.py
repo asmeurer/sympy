88306a2681d1ded6cbb954eab72196b685a212f2
Aaron Meurer <asmeurer@gmail.com>
2009-06-06 15:32:19 -0600

    Added function distribute, which distributes multiplication over addition.
    This is for issue 1455.  It only distributes on the top level.  Also added tests.

--- sympy/simplify/simplify.py
+++ sympy/simplify/simplify.py
@@ -919,6 +919,59 @@ def radsimp(expr):
 
     return n/d
 
+def distribute(expr):
+    """
+    Recursively takes a Mul with Adds and turns it into just an Add of Muls
+    This should probably be refactored into expand.  See Issue 1455.  Note that
+    this is slower than just expand, so unless you only want distribution, use
+    that.  The advantage is that it will not do things like expand logs and
+    exponentials.  It only distributes on the top level, i.e., it doesn't do
+    anything with things like sin(x*(y+z)).
+
+    Examples:
+    >>> from sympy import *
+    >>> x, y, z, a, b = symbols('xyzab')
+    >>> distribute(x*(y+z)*(a+b))
+    a*x*y + a*x*z + b*x*y + b*x*z
+
+    >>> expand(log(x**2)*(a+b))
+    2*a*log(x) + 2*b*log(x)
+    >>> distribute(log(x**2)*(a+b))
+    a*log(x**2) + b*log(x**2)
+    >>> expand(exp(x+y)*(a+b))
+    a*exp(x)*exp(y) + b*exp(x)*exp(y)
+    >>> distribute(exp(x+y)*(a+b))
+    a*exp(x + y) + b*exp(x + y)
+
+    >>> distribute(sin(x*(y+z)))
+    sin(x*(y + z))
+    """
+    if any(not getattr(i, 'is_commutative', False) for i in expr.atoms(Symbol)):
+        # TODO: non_commutative case needs to be implemented
+        return expr
+    tryagain = _distribute(expr)
+    while tryagain != expr:
+        expr = tryagain
+        tryagain = _distribute(expr)
+    return expr
+
+def _distribute(expr, i=0):
+    if expr.is_Add:
+        return Add(*(_distribute(i) for i in expr.args))
+    if expr.is_Mul:
+        term = expr.args[i]
+    else:
+        term = expr
+    if term.is_Add:
+        return Add(*(Mul(*(_distribute(expr.extract_multiplicatively(term)),i)) for i in term.args))
+    else:
+        i += 1
+        if i < len(expr.args):
+            return _distribute(expr, i)
+        else:
+            return expr
+
+
 def powsimp(expr, deep=False):
     """
     Usage
