7cd9718c717b78078c157046e79b58519cd3bba9
Aaron Meurer <asmeurer@gmail.com>
2009-06-29 11:40:50 -0600

    Preliminary work on Mul, that doesn't work

---
+++ constant.py
@@ -0,0 +1,64 @@
+from sympy import Basic, Symbol, S, sympify, any, Mul
+
+
+class Constant(Symbol):
+    """
+    Constant Symbol.
+
+    This class represents an arbirary constant Symbol.
+    That means that it will absorb anything that it isn't independent of.
+    """
+    is_Constant = True
+    is_commutative = True
+    name = 'Constant'
+
+    def __new__(cls, name, *args, **assumptions):
+        args = map(sympify, args)
+        return Basic.__new__(cls, args)
+
+    def __mul__(self, other):
+        if other.is_Mul:
+            # Mul case
+            keep = []
+            for i in Mul.args:
+                if any((i.has(t) for t in self.args)):
+                    keep.append(i)
+            return Mul(self, *keep)
+        else:
+            # Other case
+            if any((other.has(*t) for t in self.args)):
+                return Mul(self,other)
+            else:
+                return self
+
+    def __rmul__(self, other):
+        if other.is_Mul:
+            # Mul case
+            keep = []
+            for i in Mul.args:
+                if any((i.has(t) for t in self.args)):
+                    keep.append(i)
+            return Mul(self, *keep)
+        else:
+            # Other case
+            if any((other.has(*t) for t in self.args)):
+                return Mul(self,other)
+            else:
+                return self
+
+x = Symbol('x')
+y = Symbol('y')
+a = Constant('C',x)
+# We want a (Constant) below to absorb the y's, but not the x's
+print 'y*a', y*a
+print 'x*a', x*a
+print 'a*y', a*y
+print 'a*x', a*x
+print 'x*y*a', x*y*a
+print 'a*x*y', a*x*y
+print 'x*y*a', x*y*a
+print 'x*a*y', x*a*y
+print 'y*a*x', y*a*x
+print 'a*y*(y+1)', a*y*(y+1)
+print 'y*a*(y+1)', a*y*(y+1)
+print 'y*(y+1)*a', y*(y+1)*a
