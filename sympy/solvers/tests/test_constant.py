64593786037c8cae3e7bbbcaf29c6136f08db162
Aaron Meurer <asmeurer@gmail.com>
2009-07-21 22:17:33 -0600

    Get all existing tests to pass!
    This means the renumbering works (it had to be refactored into a separate function
    that is called separately).  Also, one obscure test had to be xfailed (f(C1, C1, x))
    because it would be too difficult to make it not return f(C1, C2, x).  dsolve will
    never return something like this, and the result is technically correct anyway.

--- sympy/solvers/tests/test_constant.py
+++ sympy/solvers/tests/test_constant.py
@@ -1,6 +1,8 @@
 from sympy import sin, exp, Function, Symbol, S, Pow, Eq, I, sinh, cosh, acos,\
-cos, log, Rational
+cos, log, Rational, sqrt
 from sympy.simplify.simplify import constantsimp
+from sympy.utilities.pytest import XFAIL
+
 
 x = Symbol('x')
 y = Symbol('y')
@@ -91,28 +93,33 @@ def test_constant_function():
     assert constantsimp(f(C2, C1), x, 2) == C1
     assert constantsimp(f(C2, C2), x, 2) == C1
     assert constantsimp(f(C1, x), x, 1) == f(C1, x)
-    assert constantsimp(f(C1, C1, x), x, 1) == f(C1, C1, x)
     assert constantsimp(f(C1, y), x, 1) == C1
     assert constantsimp(f(y, C1), x, 1) == C1
     assert constantsimp(f(C1, y, C2), x, 2) == C1
+
+@XFAIL
+def test_constant_function_multiple():
+    # The rules to not renumber in this case would be too complicated, and
+    # dsolve is not likely to ever encounter anything remotely like this.
+    assert constantsimp(f(C1, C1, x), x, 1) == f(C1, C1, x)
 
 def test_constant_multiple():
     assert constantsimp(C1*2 + 2, x, 1) == C1
     assert constantsimp(x*2/C1, x, 1) == C1*x
     assert constantsimp(C1**2*2 + 2, x, 1) == C1
-    assert constantsimp(sin(2*C1) + x + sqrt(2), x, 1) == C1
-    assert constantsimp(2*C1 + C2, x, 2) == C2
+    assert constantsimp(sin(2*C1) + x + sqrt(2), x, 1) == C1 + x
+    assert constantsimp(2*C1 + C2, x, 2) == C1
 
 def test_ode_solutions():
     # only a few examples here, the rest will be tested in the actual dsolve tests
     assert constantsimp(C1*exp(2*x)+exp(x)*(C2+C3), x, 3) == C1*exp(x)+C2*exp(2*x)
     assert constantsimp(Eq(f(x),I*C1*sinh(x/3) + C2*cosh(x/3)), x, 2) == \
-    Eq(f(x), C1*cosh(x/3) + C2*sinh(x/3))
+    Eq(f(x), C1*sinh(x/3) + C2*cosh(x/3))
     assert constantsimp(Eq(f(x),acos((-C1)/cos(x))), x, 1) == \
     Eq(f(x),acos(C1/cos(x)))
     assert constantsimp(Eq(log(f(x)/C1) + 2*exp(x/f(x)), 0), x, 1) == \
     Eq(log(C1*f(x)) + 2*exp(x/f(x)), 0)
-    assert constantsimp(Eq(log(x*2**Rational(1,2)*(1/x)**Rational(1,2)*f(x)**Rational(1,2)/C1) + x**2/(2*f(x)**2), 0), x, 1) == Eq(log(C1*x*(1/x)**Rational(1,2)*f(x)**Rational(1,2)) + x**2/(2*f(x)**2), 0)
-
-
+    assert constantsimp(Eq(log(x*2**Rational(1,2)*(1/x)**Rational(1,2)*f(x)\
+    **Rational(1,2)/C1) + x**2/(2*f(x)**2), 0), x, 1) == \
+    Eq(log(C1*x*(1/x)**Rational(1,2)*f(x)**Rational(1,2)) + x**2/(2*f(x)**2), 0)
 
