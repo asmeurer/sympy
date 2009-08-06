7780501e2e2a2fa3d3b13c821045e2608a17b4b8
Chris Smith <smichr@gmail.com>
2009-08-06 15:44:09 +0545

    atoms docstring and reapply extract minus sign

--- sympy/core/basic.py
+++ sympy/core/basic.py
@@ -745,34 +745,70 @@ class Basic(AssumeMeths):
         return StrPrinter.doprint(self)
 
     def atoms(self, *types):
-        """Returns the atoms that form the current object.
-           An atom is the smallest piece in which we can divide an
-           expression.
+        """Returns the atoms that form the current object. 
 
+           By defaults, only objects that are truly atomic and can't
+           be divided into smaller pieces are returned: symbols, numbers,
+           and number symbols like I and pi. It is possible to request
+           atoms of any type, however, as demonstrated below.
 
-           Examples:
+           Examples::
 
            >>> from sympy import *
            >>> x,y = symbols('xy')
+           >>> sorted((1+x+2*sin(y+I*pi)).atoms())
+           [1, 2, pi, I, x, y]
 
-           >>> sorted((x+y**2 + 2*x*y).atoms())
-           [2, x, y]
 
-           You can also filter the results by a given type(s) of object:
+           If one or more types are given, the results will be contain only those
+           types of atoms:
 
-           >>> sorted((x+y+2+y**2*sin(x)).atoms(Symbol))
+           Examples::
+
+           >>> sorted((1+x+2*sin(y+I*pi)).atoms(Symbol))
            [x, y]
 
-           >>> sorted((x+y+2+y**3*sin(x)).atoms(Number))
-           [2, 3]
+           >>> sorted((1+x+2*sin(y+I*pi)).atoms(Number))
+           [1, 2]
 
-           >>> sorted((x+y+2+y**2*sin(x)).atoms(Symbol, Number))
-           [2, x, y]
+           >>> sorted((1+x+2*sin(y+I*pi)).atoms(Number, NumberSymbol))
+           [1, 2, pi]
 
-           Or by a type of on object in an impliciy way:
+           >>> sorted((1+x+2*sin(y+I*pi)).atoms(Number, NumberSymbol, I))
+           [1, 2, pi, I]
 
-           >>> sorted((x+y+2+y**2*sin(x)).atoms(x))
-           [x, y]
+           Note that I (imaginary unit) and zoo (complex infinity) are special
+           types of number symbols and are not part of the NumberSymbol class.
+
+           The type of the object can be specified implicitly, by supplying
+           an object having the type of interest:
+
+           >>> sorted((1+x+2*sin(y+I*pi)).atoms(x)) # x is a Symbol
+           [x, y]
+
+
+           Be careful to check your assumptions when using the implicit option
+           since S(1).is_Integer = True but type(S(1)) is One, a special type 
+           of sympy atom, while type(S(2)) is type Integer and will find all
+           integers in an expression:
+
+           >>> sorted((1+x+2*sin(y+I*pi)).atoms(S(1)))
+           [1]
+
+           >>> sorted((1+x+2*sin(y+I*pi)).atoms(S(2)))
+           [1, 2]
+
+           Finally, arguments to atoms() can select more than atomic atoms: any
+           sympy type (loaded in core/__init__.py) can be listed as an argument 
+           and those types of "atoms" as found in scanning the arguments of the
+           expression nonrecursively:
+
+           >>> sorted((1+x+2*sin(y+I*pi)).atoms(Function))
+           [sin(y + pi*I)]
+
+           >>> sorted((1+x+2*sin(y+I*pi)).atoms(Mul))
+           [2*sin(y + pi*I)]
+
 
         """
 
@@ -2018,9 +2054,13 @@ class Basic(AssumeMeths):
                 elif positive_args < negative_args:
                     return True
             elif self.is_Mul:
+                # We choose the one with an odd number of minus signs
                 num, den = self.as_numer_denom()
-                if den != 0:
-                    return num.could_extract_minus_sign()
+                args = (list(num.args) if num.is_Mul else [num]) + \
+                       (list(den.args) if den.is_Mul else [den])
+                arg_signs = [arg.could_extract_minus_sign() for arg in args]
+                negative_args = filter(None, arg_signs)
+                return len(negative_args) % 2 == 1
 
             # As a last resort, we choose the one with greater hash
             return hash(self) < hash(negative_self)
