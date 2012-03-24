dcf88fbbd26b4b69a3dc8e04f42830165fd8f4ad
Aaron Meurer <asmeurer@gmail.com>
2012-03-23 18:07:46 -0600

    Continue to test raises() with string as long as we support it

--- sympy/utilities/tests/test_pytest.py
+++ sympy/utilities/tests/test_pytest.py
@@ -1,13 +1,20 @@
 from sympy.utilities.pytest import raises
 
-def test_okay_if_expected_exception():
+def test_raises_okay_if_expected_exception():
     def f():
         raise ValueError()
     raises(ValueError, f)
+    raises(ValueError, "raise ValueError")
 
 def test_not_okay_if_no_exception():
     try:
         raises(Exception, lambda: 1+1)
+        assert False
+    except Exception, e:
+        assert str(e) == "DID NOT RAISE"
+
+    try:
+        raises(Exception, '1 + 1')
         assert False
     except Exception, e:
         assert str(e) == "DID NOT RAISE"
@@ -21,8 +28,14 @@ def test_not_okay_if_wrong_exception():
     except ValueError:
         pass
 
+    try:
+        raises(TypeError, "raise ValueError")
+        assert False
+    except ValueError:
+        pass
+
 # Now we can use raises() instead of try/catch
 # to test that a specific exception class is raised
 
 def test_second_argument_should_be_callable_or_string():
-    raises(TypeError, lambda: raises("irrelevant", 42))
+    raises(TypeError, lambda: raises(Exception, 42))
